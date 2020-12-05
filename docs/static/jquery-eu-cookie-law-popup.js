9d36043030716ab8a851fe90ee86e7ef11f9ad8c
RedazioneFPA <39738669+RedazioneFPA@users.noreply.github.com>
2020-12-05 23:01:17 +0100

    Update jquery-eu-cookie-law-popup.js

--- docs/static/jquery-eu-cookie-law-popup.js
+++ docs/static/jquery-eu-cookie-law-popup.js
@@ -28,7 +28,7 @@ $.fn.euCookieLawPopup = (function() {
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	// PARAMETERS (MODIFY THIS PART) //////////////////////////////////////////////////////////////
 	_self.params = {
-		cookiePolicyUrl : 'https://manuale-libro-firma-2-0.readthedocs.io/it/latest/privacy.html',
+		cookiePolicyUrl : 'https://libro-bianco-cantiere-smartcity-fpa-2020.readthedocs.io/it/latest/privacy.html',
 		popupPosition : 'top',
 		colorStyle : 'default',
 		compactStyle : false,
